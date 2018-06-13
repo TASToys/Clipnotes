642105ce06e1d6eadc5caa84c3a6368318c07d62
MediaMagnet <nathanec@gmail.com>
2018-06-12 21:22:52 -0500

    markers can be more than a word now.

--- clipnotes.rb
+++ clipnotes.rb
@@ -7,49 +7,55 @@ require 'pg'
 require 'rest-client'
 require 'oj'
 require_relative 'defaults.rb'
-require_relative 'plugins/http_server'
 @uptime = nil
 
 def livetime
-	chan = RestClient.get "https://api.twitch.tv/helix/streams?user_id=#{configatron.twitch.channel}", {:'Client-ID' => configatron.twitch.clientid}
+	chan = RestClient.get "https://api.twitch.tv/helix/streams?user_id=#{configatron.twitch.channel}", { :'Client-ID' => configatron.twitch.clientid }
+
 	inchan = Oj.load(chan).fetch('data').first.fetch('started_at')
 	timestart = Time.parse(inchan)
 	timecurrent = Time.now.utc
 	mathtime = (timecurrent.to_i - timestart.to_i).abs
-	uptime = Time.at(mathtime).utc.strftime("%H:%M:%S")
+	@uptime = Time.at(mathtime).utc.strftime("%H:%M:%S")
 end
 
 class Mark
 	include Cinch::Plugin
 	include Cinch::Commands
 
-	command :mark, {arg1: :string},
-		summary: "Make a marker for a highlight you want to make later.",
-		discription: %{
-		use !clip to make a marker
-		}
+	command :clip, {
+		arg1: :text
+	}, 
 
-	def mark(m,arg1)
+	summary: "Make a cliper for a highlight you want to make later.", 
+	discription: %{
+ use !clip to make a markers
+	}
+
+	def clip(m, arg1)
 		db = Sequel.connect("postgres://#{configatron.sql.user}:#{configatron.sql.pass}@localhost:5432/clipnotes")
+
 		markers = db.from(:markers)
-		clipnote = "#{arg1}"
+		clipnote = arg1
 		clipuser = m.user.nick
 		cliptime = livetime
 		attime = Time.now.to_s
-		m.reply "#{m.user.nick}, A marker was created at #{cliptime} for #{clipnote}."
-		markers.insert(user: clipuser, note: clipnote, uptime: cliptime, created_at: attime)
+		m.reply "#{m.user.nick}, A marker was created at #{cliptime} for \"#{clipnote}\"."
+
+		markers.insert(user: clipuser, note: clipnote, uptime: cliptime, created_at: attime, updated_at: attime)
 	end
 end
 
+
 bot = Cinch::Bot.new do
-  configure do |c|
+	configure do |c|
 		c.server = "irc.chat.twitch.tv"
 		c.password = configatron.twitch.oauth
 		c.channels = [configatron.twitch.irc]
 		c.plugins.plugins = [Mark]
 		c.user = configatron.irc.nick
 		c.nick = configatron.irc.nick
-  end
+	end
 end
 
 bot.start
---
+++ dbmake.rb
@@ -0,0 +1,14 @@
+require 'configatron'
+require 'pg'
+require 'sequel'
+require_relative 'defaults.rb'
+
+DB = Sequel.connect("postgres://#{configatron.sql.user}:#{configatron.sql.pass}@localhost:5432/clipnotes")
+
+DB.create_table :markers do
+	primary_key :id
+	String :user
+	String :note
+	String :uptime
+	datetime :created_at
+end
